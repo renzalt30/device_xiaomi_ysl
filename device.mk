92c5b0d741fa8821d81c836b48ad14016d6e939f
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2021-10-29 08:33:16 +0530

    ysl: Speed preopt SysUI & Settings
    - Add Google dialer
    
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

--- device.mk
+++ device.mk
@@ -195,6 +195,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# Hacks
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI \
+    GoogleDialer
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
