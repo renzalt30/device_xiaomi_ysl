eb01c904e810ea4e999646c54e02decb76a72890
AL prjkt <kamu62187@gmail.com>
2024-04-16 14:57:22 +0000

    ysl: Fixup
    Signed-off-by: AL prjkt <kamu62187@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 git clone --depth=1 https://github.com/ALprjkt/vendor_xiaomi_ysl -b 13 vendor/xiaomi/ysl
 git clone --depth=1 https://github.com/ALprjkt/kernel_xiaomi_ysl -b main kernel/xiaomi/ysl
 git clone --depth=1 https://github.com/nekoprjkt/vendor -b main vendor/dolby/ds
+git clone --depth=1 https://github.com/nekoprjkt/aosp-clang -b main prebuilts/clang/host/linux-x86/clang-aosp
